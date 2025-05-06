262a54f66088df48e59daf6a5863a20bfd95caff
Aagaman Luitel <spewwerrier0777@gmail.com>
2025-05-06 19:40:49 +0545

    a django server

---
+++ sentinel/server/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from django.template import loader
+
+def index(request: HttpRequest):
+    return render(request, 'server/index.html')
+
+def log(request: HttpRequest):
+    return HttpResponse("logging bzzt bzzt")
