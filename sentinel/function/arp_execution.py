0eeba83db4d4dcfa74745c01705ee14dba2f23ae
Aagaman Luitel <spewwerrier0777@gmail.com>
2025-06-01 20:50:17 +0545

    fix arp scanner

--- sentinel/function/arp_execution.py
+++ sentinel/function/arp_execution.py
@@ -1,9 +1,11 @@
 from scapy.all import Ether, ARP, srp
+import socket
 
-# do we need to manually assign ip? We only know 192.168 is known
-# 192.168.2^8 * 2^ 8 = 65536 ips. These much ip we need to calculate????????????
-# Example IP range to scan
-ip_range = "192.168.101.5/24"
+ipaddr = socket.gethostbyname(socket.gethostname())
+# seems like you only need first 3 octets
+ip_range = ".".join(ipaddr.split(".")[:-1] + ["1/24"])
+
+# ip_range = "192.168.101.1/24"
 
 # Send ARP requests
 packet = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=ip_range)
@@ -16,4 +18,3 @@ ans, _ = srp(packet, timeout=2, verbose=0)
 # Print IP MAC per line (plain)
 for sent, received in ans:
     print(f"{received.psrc} {received.hwsrc}")
-
